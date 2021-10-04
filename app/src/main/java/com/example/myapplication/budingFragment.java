d30b60670594bdc93517446c4e52bbf25b7199aa
Haru-Malfoy <2388478782@qq.com>
2021-10-04 19:39:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/budingFragment.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.app.Fragment;
+
+public class budingFragment extends Fragment {
+    public budingFragment(){
+
+    }
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
